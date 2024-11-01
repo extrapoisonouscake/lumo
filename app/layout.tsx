e144dde7326aa8f8de5633b5653236cac5a723c0
Vlad Gubarev <extrapoisonouscake@gmail.com>
2024-11-01 09:56:20 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import { LogOut } from "@/components/layout/log-out";
 import { ReactNode } from "react";
 import { Toaster } from "react-hot-toast";
 import "./globals.css";
-import { Providers } from "./providers";
+import { Providers } from "./providers.tsx";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
