1b422b13536a1558fd0b608882aa9ac0f002fc6c
Vlad Gubarev <extrapoisonouscake@gmail.com>
2024-11-04 10:07:41 -0800

    clean up

--- parsing/fetchMyEd.ts
+++ parsing/fetchMyEd.ts
@@ -1,11 +1,19 @@
+import { getAuthCookies } from "@/helpers/getAuthCookies";
+import { MyEdCookieStore } from "@/helpers/MyEdCookieStore";
 import { MyEdFetchEndpoints } from "@/types/myed";
+import { cookies } from "next/headers";
 import "server-only";
-import { fetchMyEdPageHTML } from "./fetchMyEdPageHTML";
+import { sendMyEdRequest } from "./sendMyEdRequest";
 import { parseSubjects } from "./subjects";
 const endpointToFunction = {
   subjects: parseSubjects,
 } as const satisfies Record<MyEdFetchEndpoints, (html: string) => any>;
 export async function fetchMyEd(endpoint: MyEdFetchEndpoints) {
-  const html = await fetchMyEdPageHTML(endpoint);
+  const cookieStore = new MyEdCookieStore(cookies());
+  let response = await sendMyEdRequest(endpoint, getAuthCookies(cookieStore));
+  if (!response.ok) {
+    throw response;
+  }
+  const html = await response.text();
   return endpointToFunction[endpoint](html);
 }
