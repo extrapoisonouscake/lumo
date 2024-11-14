528edb5ce13686a0dbd8b6fa3b34ed4ae93d2bf2
Vladislav Gubarev <extrapoisonouscake@gmail.com>
2024-11-14 13:56:20 -0800

    change sidebar icon on mobile

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -2,7 +2,7 @@
 
 import { Slot } from "@radix-ui/react-slot";
 import { VariantProps, cva } from "class-variance-authority";
-import { PanelLeft } from "lucide-react";
+import { Menu, PanelLeft } from "lucide-react";
 import * as React from "react";
 
 import { Button } from "@/components/ui/button";
@@ -263,8 +263,8 @@ const SidebarTrigger = React.forwardRef<
   React.ElementRef<typeof Button>,
   React.ComponentProps<typeof Button>
 >(({ className, onClick, ...props }, ref) => {
-  const { toggleSidebar } = useSidebar();
-
+  const { toggleSidebar, isMobile } = useSidebar();
+  const Icon = isMobile ? Menu : PanelLeft;
   return (
     <Button
       ref={ref}
@@ -278,7 +278,7 @@ const SidebarTrigger = React.forwardRef<
       }}
       {...props}
     >
-      <PanelLeft />
+      <Icon />
       <span className="sr-only">Toggle Sidebar</span>
     </Button>
   );
