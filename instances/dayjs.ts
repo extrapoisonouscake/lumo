cb95d86b9272cfb953a845a74d4ef9357571662c
Vladislav Gubarev <extrapoisonouscake@gmail.com>
2024-11-14 12:58:15 -0800

    f

--- instances/dayjs.ts
+++ instances/dayjs.ts
@@ -3,10 +3,21 @@ import customParseFormat from "dayjs/plugin/customParseFormat";
 import isBetween from "dayjs/plugin/isBetween";
 import timezone from "dayjs/plugin/timezone";
 import utc from "dayjs/plugin/utc";
-export default dayjs;
+
 dayjs.extend(customParseFormat);
 dayjs.extend(isBetween);
 dayjs.extend(timezone);
 dayjs.extend(utc);
 export const INSTANTIATED_TIMEZONE = "America/Vancouver";
 dayjs.tz.setDefault(INSTANTIATED_TIMEZONE);
+const timezonedDayjs = (...args: any[]) => {
+  return dayjs(...args).tz();
+};
+
+const timezonedUnix = (value: number) => {
+  return dayjs.unix(value).tz();
+};
+
+timezonedDayjs.unix = timezonedUnix;
+
+export default timezonedDayjs;
