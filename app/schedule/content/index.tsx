2374ca45e5f1a9598fe94400f63106cd539453c1
Vlad Gubarev <extrapoisonouscake@gmail.com>
2024-11-20 03:26:45 +0530

    Update index.tsx

--- app/schedule/content/index.tsx
+++ app/schedule/content/index.tsx
@@ -2,7 +2,7 @@ import { ErrorCard } from "@/components/misc/error-card";
 import { ReloginWrapper } from "@/components/relogin-wrapper";
 import { Card } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
-import { timezonedDayJS } from "@/instances/dayjs";
+import { dayjs,timezonedDayJS } from "@/instances/dayjs";
 import { fetchMyEd, sessionExpiredIndicator } from "@/parsing/fetchMyEd";
 import { MyEdEndpointsParams } from "@/types/myed";
 import { ComponentProps } from "react";
@@ -11,7 +11,7 @@ import { ScheduleTable } from "./table";
 export async function ScheduleContent({ day }: { day: string | undefined }) {
   const params: MyEdEndpointsParams<"schedule"> = {};
   if (day) {
-    params.day = timezonedDayJS(day, "MM-DD-YYYY").format("DD/MM/YYYY");
+    params.day = dayjs(day, "MM-DD-YYYY").format("DD/MM/YYYY");
   }
   const data = await fetchMyEd("schedule", params);
   if (data === sessionExpiredIndicator)
@@ -27,7 +27,7 @@ export async function ScheduleContent({ day }: { day: string | undefined }) {
   }
   return (
     <>
-      {data && timezonedDayJS(day).day() === 5 && (
+      {data && dayjs(day).day() === 5 && (
         <h3>
           Shortened day schedule:{" "}
           <span className="text-bold">{data.weekday}</span>
@@ -45,7 +45,7 @@ export function ScheduleContentSkeleton({
 }: ComponentProps<typeof ScheduleContent>) {
   return (
     <>
-      {timezonedDayJS(day).day() === 5 && (
+      {dayjs(day).day() === 5 && (
         <div className="flex items-center gap-2">
           <h3>Shortened day schedule:</h3>
           <Skeleton>
