afd0bf2cb358d9899319432018935d36680ffce3
Vlad Gubarev <extrapoisonouscake@gmail.com>
2024-11-20 22:38:04 -0800

    Update component.tsx

--- components/layout/theme-toggle/component.tsx
+++ components/layout/theme-toggle/component.tsx
@@ -19,7 +19,7 @@ export function ThemeToggleComponent() {
     <SidebarMenuItem>
       <Select value={theme} onValueChange={setTheme}>
         <SelectTrigger asChild>
-          <SidebarMenuButton>
+          <SidebarMenuButton shouldCloseSidebarOnMobile={false}>
             <Sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
             <Moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
             Theme
