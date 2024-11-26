6659e67aa5636ead208f53e886ddab933834281e
Vlad Gubarev <extrapoisonouscake@gmail.com>
2024-11-26 08:19:20 -0800

    Update index.tsx

--- app/_components/announcements/index.tsx
+++ app/_components/announcements/index.tsx
@@ -4,6 +4,7 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { ErrorCard } from "@/components/misc/error-card";
 import { Card } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 import { isKnownSchool } from "@/constants/schools";
@@ -14,25 +15,11 @@ export async function Announcements() {
   const school = cookies().get("schoolId")?.value;
   if (!school || !isKnownSchool(school)) return null;
   const data = await getAnnouncements(school);
-  if (!data)
-    return (
-      <div className="w-full flex justify-center">
-        <Card className="w-full max-w-[450px] p-3 gap-1">
-          <AppleEmoji
-            value="💨"
-            textClassName="text-3xl leading-8"
-            imageClassName="size-[30px]"
-          />
-          <p className="text-muted-foreground text-sm">
-            No announcements for today.
-          </p>
-        </Card>
-      </div>
-    );
+  
   return (
     <div className="flex flex-col gap-2">
       <h3 className="text-sm">Announcements</h3>
-      <AnnouncementsAccordions data={data} />
+      {data?<AnnouncementsAccordions data={data} />:<ErrorCard emoji="💨" message="No announcements yet."/>
     </div>
   );
 }
