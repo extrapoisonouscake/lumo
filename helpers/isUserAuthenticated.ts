d9be475251737c20873fde216a1980b52fdc19ac
Vlad Gubarev <extrapoisonouscake@gmail.com>
2024-12-03 13:58:24 -0800

    Update isUserAuthenticated.ts

--- helpers/isUserAuthenticated.ts
+++ helpers/isUserAuthenticated.ts
@@ -1,7 +1,7 @@
-import { RequestCookies } from "next/dist/compiled/@edge-runtime/cookies";
+import { RequestCookies, ReadonlyRequestCookies } from "next/dist/compiled/@edge-runtime/cookies";
 import { getFullCookieName } from "./getFullCookieName";
 
-export function isUserAuthenticated(cookieStore: RequestCookies) {
+export function isUserAuthenticated(cookieStore: RequestCookies|ReadonlyRequestCookies) {
   const isAuthenticated =
     cookieStore.has(getFullCookieName("username")) &&
     cookieStore.has(getFullCookieName("password"));
