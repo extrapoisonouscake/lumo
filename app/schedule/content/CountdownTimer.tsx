437da38870a63b4fcc58bacbea422ecd2b5ba171
Vlad Gubarev <extrapoisonouscake@gmail.com>
2024-12-20 11:05:21 -0800

    Update CountdownTimer.tsx

--- app/schedule/content/CountdownTimer.tsx
+++ app/schedule/content/CountdownTimer.tsx
@@ -7,18 +7,17 @@ export function CountdownTimer({
   if (!timeToNextSubject || timeToNextSubject > 1000 * 60 * 60) return null;
   const [key, setKey] = useState(Date.now());
   useEffect(() => {
-    console.log({ timeToNextSubject });
     setKey(Date.now());
   }, [timeToNextSubject]);
-  console.log({ key });
+  
   const seconds = useMemo(
     () => Math.max(Math.floor(timeToNextSubject / 1000), 0),
     [key]
   );
   const minutes = Math.max(Math.floor(seconds / 60), 0);
   return (
-    <div>
-      {`${minutes}`.padStart(2, "0")}:{`${seconds % 60}`.padStart(2, "0")}
-    </div>
+    <p>
+      {`${minutes}`.padStart(2, "0")}:{`${seconds % 60}`.padStart(2, "0")} left
+    </p>
   );
 }
