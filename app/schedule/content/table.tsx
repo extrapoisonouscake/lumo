c6c4aa1aab6389b3d72939e9d3965aa70175bd86
Vladislav Gubarev <extrapoisonouscake@gmail.com>
2025-01-30 00:32:28 -0800

    fix

--- app/schedule/content/table.tsx
+++ app/schedule/content/table.tsx
@@ -248,16 +248,17 @@ export function ScheduleTable({
   });
   const getRowClassName = useMemo(
     () => (row: Row<ScheduleSubjectRow>) => {
+      const shouldBeClickable =
+        row.original.type === "subject" &&
+        !(row.original.name === TEACHER_ADVISORY_ABBREVIATION);
       return cn({
         "hover:bg-[#f9f9fa] dark:hover:bg-[#18181a] sticky [&:not(:last-child)>td]:border-b [&+tr>td]:border-t-0 top-0 bottom-0 bg-background shadow-[0_-1px_0_#000,_0_1px_0_var(hsl(--border))] [&>td:first-child]:relative [&>td:first-child]:overflow-hidden [&>td:first-child]:after:w-1 [&>td:first-child]:after:h-full [&>td:first-child]:after:bg-blue-500 [&>td:first-child]:after:absolute [&>td:first-child]:after:left-0 [&>td:first-child]:after:top-0":
           timezonedDayJS().isBetween(
             row.original.startsAt,
             row.original.endsAt
           ),
-        "cursor-pointer": row.original.type === "subject",
-        "[&>td]:py-2":
-          row.original.type !== "subject" ||
-          row.original.name === TEACHER_ADVISORY_ABBREVIATION,
+        "cursor-pointer": shouldBeClickable,
+        "[&>td]:py-2": !shouldBeClickable,
       });
     },
 
