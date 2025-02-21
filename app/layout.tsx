48f4507ed416297d14b88d5e83831dbf069ffa85
Vlad Gubarev <extrapoisonouscake@gmail.com>
2025-02-21 13:26:16 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/sonner";
 import { WEBSITE_TITLE } from "@/constants/website";
 import { cn } from "@/helpers/cn";
 import { GeistSans } from "geist/font/sans";
-import { Metadata } from "next";
+import { Metadata,Viewport } from "next";
 import { ReactNode } from "react";
 import "./globals.css";
 import { Providers } from "./providers";
@@ -11,9 +11,9 @@ export const metadata: Metadata = {
   title: {
     default: WEBSITE_TITLE,
     template: `%s | ${WEBSITE_TITLE}`,
-  },
-viewport:{maximumScale:1,userScalable:false}
+  }
 };
+export const viewport:Viewport={maximumScale:1}
 export default async function RootLayout({
   children,
 }: {
