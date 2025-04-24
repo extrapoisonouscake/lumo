d2fccb80119f8004fdfb49b61de2cd1c291c863c
Vladislav Gubarev <extrapoisonouscake@gmail.com>
2025-04-24 10:23:40 -0700

    change drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
   out: "./db/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.SESSION_POOLER_DATABASE_URL!,
   },
 });
