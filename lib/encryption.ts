361d93c2665bfe94015e3f5750681b179a9a6429
Vladislav Gubarev <extrapoisonouscake@gmail.com>
2025-06-10 21:26:52 -0700

    fix: remove faulty string

--- lib/encryption.ts
+++ lib/encryption.ts
@@ -4,7 +4,7 @@ if (!MYED_CREDENTIALS_ENCRYPTION_KEY) {
   throw new Error("MYED_CREDENTIALS_ENCRYPTION_KEY is not set");
 }
 // Define a fixed key (must be 256-bit)
-const key = CryptoJS.enc.Hex.parse(MYED_CREDENTIALS_ENCRYPTION_KEY + "1");
+const key = CryptoJS.enc.Hex.parse(MYED_CREDENTIALS_ENCRYPTION_KEY);
 
 // Synchronous encryption
 export const encryption = {
