64728c21194b68dcf95fe7b1b31839009ddd11d1
Vladislav Gubarev <extrapoisonouscake@gmail.com>
2025-09-02 18:32:29 -0700

    fix: styles

--- app/(authenticated)/classes/content.tsx
+++ app/(authenticated)/classes/content.tsx
@@ -34,35 +34,33 @@ export function SubjectsPageContent() {
   );
 
   return (
-    <div className="flex flex-col gap-2">
-      <QueryWrapper query={query} skeleton={<SubjectsPageSkeleton />}>
-        {(response) => {
-          return (
-            <LoadedContent
-              response={{
-                ...response,
-                subjects: {
-                  ...response.subjects,
-                  main: response.subjects.main.map((subject) => {
-                    const academics =
-                      subjectSummaries.data[subject.id]?.academics;
-                    return {
-                      ...subject,
-                      average:
-                        academics?.posted.overall ?? academics?.running.overall,
-                    };
-                  }),
-                },
-              }}
-              //*timewise
-              currentTermIndex={currentTermIndex}
-              year={year}
-              term={term}
-            />
-          );
-        }}
-      </QueryWrapper>
-    </div>
+    <QueryWrapper query={query} skeleton={<SubjectsPageSkeleton />}>
+      {(response) => {
+        return (
+          <LoadedContent
+            response={{
+              ...response,
+              subjects: {
+                ...response.subjects,
+                main: response.subjects.main.map((subject) => {
+                  const academics =
+                    subjectSummaries.data[subject.id]?.academics;
+                  return {
+                    ...subject,
+                    average:
+                      academics?.posted.overall ?? academics?.running.overall,
+                  };
+                }),
+              },
+            }}
+            //*timewise
+            currentTermIndex={currentTermIndex}
+            year={year}
+            term={term}
+          />
+        );
+      }}
+    </QueryWrapper>
   );
 }
 function SubjectsPageSkeleton() {
