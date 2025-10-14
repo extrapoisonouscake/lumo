54a08c1647b24aff65f25e1d5ed5e92f58905b97
Vladislav Gubarev <extrapoisonouscake@gmail.com>
2025-10-14 03:08:46 -0700

    fix: build

--- next.config.mjs
+++ next.config.mjs
@@ -1,59 +1,62 @@
-import {withSentryConfig} from "@sentry/nextjs";
+import { withSentryConfig } from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
-export default withSentryConfig({
-  eslint: {
-    ignoreDuringBuilds: true,
+export default withSentryConfig(
+  {
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+
+    reactStrictMode: false,
+
+    webpack(config) {
+      // Configure SVG imports as React components
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      });
+
+      return config;
+    },
+
+    async rewrites() {
+      return [
+        {
+          source: "/announcements/direct/:id",
+          destination: `https://${process.env.UPLOADTHING_APP_ID}.ufs.sh/f/:id`,
+        },
+      ];
+    },
   },
+  {
+    // For all available options, see:
+    // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-  reactStrictMode: false,
+    org: "lumobc",
 
-  webpack(config) {
-    // Configure SVG imports as React components
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
+    project: "lumo",
 
-    return config;
-  },
+    // Only print logs for uploading source maps in CI
+    silent: !process.env.CI,
 
-  async rewrites() {
-    return [
-      {
-        source: "/announcements/direct/:id",
-        destination: `https://${process.env.UPLOADTHING_APP_ID}.ufs.sh/f/:id`,
-      },
-    ];
-  },
-}, {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-  org: "myedplus",
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
 
-  project: "javascript-nextjs",
+    // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+    // This can increase your server load as well as your hosting bill.
+    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+    // side errors will fail.
+    // tunnelRoute: "/monitoring",
 
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
 
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  // tunnelRoute: "/monitoring",
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
-});
+    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+    // See the following for more information:
+    // https://docs.sentry.io/product/crons/
+    // https://vercel.com/docs/cron-jobs
+    automaticVercelMonitors: true,
+  }
+);
