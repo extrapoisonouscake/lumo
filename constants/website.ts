60a07a34951f72931bc643a221ad6d7b60d9d2df
Vlad Gubarev <extrapoisonouscake@gmail.com>
2025-10-29 10:19:57 -0700

    Update website.ts

--- constants/website.ts
+++ constants/website.ts
@@ -1,4 +1,4 @@
-
+import {isProduction} from "./core"
 export const WEBSITE_TITLE = "Lumo";
 
 export interface WebsitePage {
@@ -44,9 +44,10 @@ export const VISIBLE_DATE_FORMAT = "MM/DD/YYYY";
 export const VISIBLE_TIME_FORMAT = "h:mm A";
 export const APP_STORE_APP_ID = "6752838080";
 const isProductionOverride = process.env.NEXT_PUBLIC_HTTP_NODE_ENV_OVERRIDE === "production";
-export const DEFAULT_DOMAIN = isProductionOverride ? "lumobc.ca" : "localhost";
+const shouldUseProductionDomain= isProductionOverride||isProduction
+export const DEFAULT_DOMAIN = shouldUseProductionDomain ? "lumobc.ca" : "localhost";
 const NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL =
   process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL; //no other syntax allowed due to Vercel
-export const WEBSITE_ROOT = isProductionOverride
+export const WEBSITE_ROOT = shouldUseProductionDomain
   ? `https://${NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL || DEFAULT_DOMAIN}`
   : `http://${DEFAULT_DOMAIN}:3000`;
