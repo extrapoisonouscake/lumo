88d7f73dab1ef3065bf5bba0e6cc519d5c4cb51e
Vladislav Gubarev <extrapoisonouscake@gmail.com>
2025-10-29 18:33:26 -0700

    fix: s

--- utils/generate-npmrc.js
+++ utils/generate-npmrc.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
 const path = require("path");
 require("dotenv/config");
+console.log(process.env.NPM_RC, process.env, __dirname);
 fs.writeFileSync(path.join(__dirname, ".npmrc"), process.env.NPM_RC);
